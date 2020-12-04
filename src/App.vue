3e4ca56870fe8e2deee661e3f576d669200bbecb
jraynolds <jaspirian@gmail.com>
2020-12-03 18:18:29 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,123 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-spacer/>
+        Stahlbush App
+      <v-spacer/>
+    
+    </v-app-bar>
+
+    <v-main>
+
+      <v-container>
+        <v-row>
+          <v-card-subtitle class="pt-6">You are Richard Thomas, a</v-card-subtitle>
+          <v-select class="flex-grow-0" v-model="role" :items="roles" />
+          <v-card-subtitle class="pt-6">in</v-card-subtitle>
+          <v-select class="flex-grow-0" v-model="department" :items="departments" />
+        </v-row>
+
+        <Database :data="selectDataTables" :descString="filterText.worker" />
+
+      </v-container>
+
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  components: {
+    Database: () => import("@/components/Database.vue")
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    role: "Worker",
+    roles: [ "Worker", "Manager", "Admin" ],
+    department: "Landscaping",
+    departments: [ "Landscaping", "Home Improvement", "R&D" ],
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    filterText: {
+      worker: "where the only items are in your department"
+    },
+
+    data: {
+      headers: [
+        { text: 'Requisition number', align: 'start', value: 'number', width: 100 },
+        { text: 'Status', value: 'status' },
+        { text: 'Buyer assistance?', value: 'buyerAssistance', width: 50 },
+        { text: 'Item', value: 'item' },
+        { text: 'Notes', value: 'notes' },
+        { text: 'Date', value: 'date' },
+        { text: 'Department', value: 'dept' },
+        { text: 'Requester', value: 'requester' },
+        { text: 'Buyer', value: 'buyer' },
+        { text: 'Shipping Method', value: 'shipMethod' },
+        // { text: 'Amount', value: 'amount' },
+        // { text: 'Item cost', value: 'cost' },
+        { text: 'Total cost', value: "totalCost" },
+      ],
+      items: [
+        {
+          number: 1,
+          status: "REQUESTED",
+          buyerAssistance: false,
+          item: "Bag of dirt",
+          notes: "This is a bag of dirt.",
+          date: "11/29/2020",
+          dept: "Landscaping",
+          requester: "Richard T.",
+          buyer: "Home Depot",
+          shipMethod: "Standard Ground",
+          amount: 8,
+          cost: 50
+        },
+        {
+          number: 2,
+          status: "APPROVED",
+          buyerAssistance: true,
+          item: "Small shrubs",
+          notes: "Bougenvalia.",
+          date: "11/26/2020",
+          dept: "Landscaping",
+          requester: "Jasper R.",
+          buyer: "Lowe's",
+          shipMethod: "Air Mail",
+          amount: 16,
+          cost: 10
+        },
+        {
+          number: 3,
+          status: "REQUESTED",
+          buyerAssistance: false,
+          item: "Box of nails",
+          notes: "4mm.",
+          date: "11/28/2020",
+          dept: "Home Improvement",
+          requester: "James C.",
+          buyer: "Home Depot",
+          shipMethod: "Expedited",
+          amount: 100,
+          cost: .05
+        },
+      ]
+    }
+  }),
+  computed: {
+    selectDataTables() {
+      let department = this.department;
+      return {
+        headers: this.data.headers,
+        items: this.data.items.filter(item => item.dept === department)
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/Database.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Database
+      <v-card-subtitle>{{ descString }}</v-card-subtitle>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+
+    <v-data-table :headers="data.headers" :items="data.items" :search="search" title="Database">
+
+      <template v-slot:item.buyerAssistance="{ item }">
+        <v-icon>
+          {{ item.buyerAssistance ? "mdi-check" : "mdi-close" }}  
+        </v-icon>
+      </template>
+
+      <template v-slot:item.totalCost="{ item }">
+        {{ "$" + item.amount * item.cost }}
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [ "data", "descString" ],
+  data: () => ({
+    search: ""
+  })
+}
+</script>
+
+<style>
+
+</style>
